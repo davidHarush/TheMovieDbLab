fff82767501a7f7420279996a500039c9ad36dfa
David <david.harush1@gmail.com>
2024-04-19 21:38:17 +0300

    update ver to 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.david.movie.movielab"
         minSdk = 33
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.01"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
